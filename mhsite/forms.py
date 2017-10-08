ef5641979cba2df813238fb3cfa235d17d23200f
Ajesh <ajeshkumar36020@gmail.com>
2017-10-08 19:34:47 +0530

    Created Repo

---
+++ mhsite/forms.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=true)
+
+    class Meta:
+        model = User
+
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        )
+
+        def save(self, commit=True):
+            user = super(RegistrationForm, self).save(commit=False)
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.email = self.cleaned_data['email']
+            user.password1 = self.cleaned_data['password1']
+            user.password2 = self.cleaned_data['password2']
+
+            if commit:
+                user.save()
---
+++ mhsite/views.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'mhsite/index.html')
+
+
+
+
