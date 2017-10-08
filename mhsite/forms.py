f5bd7df0f05e75bf9b7dca0421c2c3fe1a3a9b6f
Ajesh <ajeshkumar36020@gmail.com>
2017-10-08 21:13:47 +0530

    Completed Registration Form

--- mhsite/forms.py
+++ mhsite/forms.py
@@ -15,15 +15,14 @@ class RegistrationForm(UserCreationForm):
             'password1',
             'password2',
         )
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.username = self.cleaned_data['email']
+        user.email = self.cleaned_data['email']
+        user.password1 = self.cleaned_data['password1']
+        user.password2 = self.cleaned_data['password2']
 
-        def save(self, commit=True):
-            user = super(RegistrationForm, self).save(commit=False)
-            user.first_name = self.cleaned_data['first_name']
-            user.last_name = self.cleaned_data['last_name']
-            user.username = self.cleaned_data['email']
-            user.password1 = self.cleaned_data['password1']
-            user.password2 = self.cleaned_data['password2']
-            user.email = self.cleaned_data['email']
-
-            if commit:
-                user.save()
+        if commit:
+            user.save()
